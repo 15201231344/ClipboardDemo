7605bceade2c758703e2d19f76e3eb098b8a83c1
yanyixin <15201231344@163.com>
2021-04-20 19:10:03 +0800

    添加项目文件。

---
+++ ClipboardDemo/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace ClipboardDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnPaste = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(82, 115);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(107, 47);
+            this.btnCopy.TabIndex = 0;
+            this.btnCopy.Text = "拷贝数据";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnPaste
+            // 
+            this.btnPaste.Location = new System.Drawing.Point(212, 115);
+            this.btnPaste.Name = "btnPaste";
+            this.btnPaste.Size = new System.Drawing.Size(107, 47);
+            this.btnPaste.TabIndex = 1;
+            this.btnPaste.Text = "粘贴数据";
+            this.btnPaste.UseVisualStyleBackColor = true;
+            this.btnPaste.Click += new System.EventHandler(this.btnPaste_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(348, 115);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(107, 47);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "清空粘贴板";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 255);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnPaste);
+            this.Controls.Add(this.btnCopy);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnPaste;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ ClipboardDemo/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClipboardDemo
+{
+    public partial class Form1 : Form
+    {
+        private static string CopyData = "拷贝的字符串";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 拷贝按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            //拷贝数据到粘贴板，仅限于当前程序，关闭程序后粘贴板内容清空
+            //Clipboard.SetDataObject(CopyData);
+
+            //拷贝数据到粘贴板，整个系统内，关闭程序后也可在任何地方粘贴
+            //只有true才可，false相当于上面方法
+            Clipboard.SetDataObject(CopyData,true);
+
+            //拷贝数据到粘贴板，整个系统内，关闭程序后也可在任何地方粘贴
+            //拷贝三次，每次间隔200ms,在使用拷贝数据的接口时，可能粘贴板正在被占用，这时会导致设置数据失败
+            //所以使用该方法，多次设置
+            //Clipboard.SetDataObject(CopyData, true,3,200);
+        }
+
+        /// <summary>
+        /// 粘贴
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnPaste_Click(object sender, EventArgs e)
+        {
+            string data;
+
+            //这四种方式是等效的，都是获取文本数据
+
+            //获取粘贴板的对象，没有数据将返回null
+            if (Clipboard.ContainsData(DataFormats.Text))
+            {
+                IDataObject dataObject = Clipboard.GetDataObject();
+
+                //将粘贴的数据转换成string
+                data = dataObject.GetData(DataFormats.Text)?.ToString();
+            }
+           
+            //直接通过GetData获取数据
+            //object objData = Clipboard.GetData(DataFormats.Text);
+
+            //if(objData!= null)
+            //{
+            //    data = objData.ToString();
+            //}
+
+            //也可以使用下面方法获取string
+            //data = Clipboard.GetText();
+
+            //也可以使用下面方法获取string
+            //data = Clipboard.GetText(TextDataFormat.Text);
+
+            MessageBox.Show("粘贴的文本未：" + data);
+        }
+
+        /// <summary>
+        /// 清空粘贴板
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Clipboard.Clear();
+        }
+
+        /// <summary>
+        /// 拷贝图片到粘贴板
+        /// </summary>
+        /// <param name="image"></param>
+        private void CopyImage(Image image)
+        {
+            Clipboard.SetImage(image);
+        }
+        /// <summary>
+        /// 粘贴图片
+        /// </summary>
+        /// <returns></returns>
+        public Image PasteImage()
+        {
+            Image image = null;
+            if (Clipboard.ContainsImage())
+            {
+                image = Clipboard.GetImage();
+            }
+            return image;
+        }
+    }
+}
